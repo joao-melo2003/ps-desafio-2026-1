e97c26f8e2b46acf4f411f97821dd7596bf2a7f9
Joao Vitor Rocha Melo <joao.r.melo2003@gmail.com>
2026-03-25 23:00:42 -0300

    feat: adiciona uma api para categorias (category)

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,23 +1,39 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Symfony\Component\HttpFoundation\Response;
 
+// Retorna sempre ok
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/profile', function (Request $request) {
         return response()->json(Auth::user(), Response::HTTP_OK);
     });
 });
 
+// protegido por admin
 Route::middleware(['auth:sanctum', 'can:admin'])->group(function () {
     Route::apiResource('/users', UserController::class);
+    // Route::apiResource('/category', CategoryController::class);
 });
 
+// cria um usuario
+Route::post('/users', [UserController::class, 'store']);
+
+// so um teste basico
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
+
+// Route::get('/category', [CategoryController::class, 'index']);
+// Route::post('/category', [CategoryController::class, 'store']);
+// Route::get('/category/{id}', [CategoryController::class, 'show']);
+// Route::put('/category/{id}', [CategoryController::class, 'update']);
+// Route::delete('/category/{id}', [CategoryController::class, 'destroy']);
+
+Route::apiResource('/category', CategoryController::class);
 
 require __DIR__.'/auth.php';
